45d6dce57a9c73cf594991fad1fe1939ddd1ae89
Invert_Ignas <148909431+IgnasFalcon@users.noreply.github.com>
2026-03-31 16:38:05 -0700

    Update rainbow.sh

--- rainbow.sh
+++ rainbow.sh
@@ -19,7 +19,7 @@ colors=(
 
 while true; do
   for c in "${colors[@]}"; do
-    echo -ne "${c}$(asciirainbow.txt)\e[0m\r"
+    echo -ne "${c}$(cat asciirainbow.txt)\e[0m\r"
     sleep 1
     clear
   done
