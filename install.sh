efafd9334bad1524fcdbc2152046a896c7aa64a1
Invert_Ignas <148909431+IgnasFalcon@users.noreply.github.com>
2026-04-01 12:34:08 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #/bin/bash
+
 clear
+
 #ERRORS
 echo $PATH | grep /.local/bin > /dev/null
 if [[ $(echo $?) == 1 ]]; then
     echo "~/.local/bin is not in path. Add it to your path and try again."
     exit 127
 fi
+
 #Installation
 mv -f rainbow.sh rainbow
 chmod +x ./rainbow
 mv -f rainbow ~/.local/bin
 mv -f asciirainbow.txt ~/.local/bin
+echo "Installing..."
+sleep 2
 echo "Installation completed successfully."
 echo "Do you want to clean up the installation? (Recommended) (Y/n)"
 read clean
@@ -18,8 +23,8 @@ if ! [[ $clean == "n" || $clean == "N" ]]; then
     cd ..
     rm -rf Rainbow-Shell
 fi
-#Finished
 
+#Finished
 echo "Finished installing. If you want to execute the script run:"
 echo "rainbow [CYCLES]"
 exit 0
