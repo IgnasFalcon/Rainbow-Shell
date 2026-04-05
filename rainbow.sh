79678e430126baa975eda260d67b48469930d846
Invert_Ignas <148909431+IgnasFalcon@users.noreply.github.com>
2026-04-05 08:24:28 -0700

    Changed program to display rainbow in one command
    Removed redundant clear command in rainbow function.
    
    Changed program to display rainbow in one command

--- rainbow.sh
+++ rainbow.sh
@@ -16,15 +16,16 @@ colors=(
 #Rainbow function
 rainbow() {
     for c in "${colors[@]}"; do
-        echo -e "${c}$(echo "ooooooooo.         .o.       ooooo ooooo      ooo oooooooooo.    .oooooo.   oooooo   oooooo     oooo ")\e[0m\r"
-        echo -e "${c}$(echo ".888   .Y88.      .888.      .888. .888b.     .8. .888.   .Y8b  d8P.  .Y8b   .888.    .888.     .8.  ")\e[0m\r"
-        echo -e "${c}$(echo " 888   .d88.     .88888.      888   8 .88b.    8   888     888 888      888   .888.   .8888.   .8.   ")\e[0m\r"
-        echo -e "${c}$(echo " 888ooo88P.     .8. .888.     888   8   .88b.  8   888oooo888. 888      888    .888  .8..888. .8.    ")\e[0m\r"
-        echo -e "${c}$(echo " 888.88b.      .88ooo8888.    888   8     .88b.8   888    .88b 888      888     .888.8.  .888.8.     ")\e[0m\r"
-        echo -e "${c}$(echo " 888  .88b.   .8.     .888.   888   8       .888   888    .88P .88b    d88.      .888.    .888.      ")\e[0m\r"
-        echo -ne "${c}$(echo "o888o  o888o o88o     o8888o o888o o8o        .8  o888bood8P.   .Y8bood8P.        .8.      .8.       ")\e[0m\r"
-        sleep 1
         clear
+        echo -e "${c}$(echo "
+ooooooooo.         .o.       ooooo ooooo      ooo oooooooooo.    .oooooo.   oooooo   oooooo     oooo 
+.888   .Y88.      .888.      .888. .888b.     .8. .888.   .Y8b  d8P.  .Y8b   .888.    .888.     .8.  
+ 888   .d88.     .88888.      888   8 .88b.    8   888     888 888      888   .888.   .8888.   .8.   
+ 888ooo88P.     .8. .888.     888   8   .88b.  8   888oooo888. 888      888    .888  .8..888. .8.    
+ 888.88b.      .88ooo8888.    888   8     .88b.8   888    .88b 888      888     .888.8.  .888.8.     
+ 888  .88b.   .8.     .888.   888   8       .888   888    .88P .88b    d88.      .888.    .888.      
+o888o  o888o o88o     o8888o o888o o8o        .8  o888bood8P.   .Y8bood8P.        .8.      .8.")\e[0m\r"
+        sleep 1
     done
 }
 
