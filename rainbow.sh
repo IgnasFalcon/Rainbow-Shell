bc15d43ee7c5f2ad3f302f20f566921b7047ef87
Invert_Ignas <148909431+IgnasFalcon@users.noreply.github.com>
2026-04-04 17:35:31 -0700

    Update rainbow.sh

--- rainbow.sh
+++ rainbow.sh
@@ -19,24 +19,31 @@ colors=(
   "\e[35m"       # Violet
 )
 
+#Rainbow function
+rainbow() {
+    for c in "${colors[@]}"; do
+        echo -e "${c}$(echo "ooooooooo.         .o.       ooooo ooooo      ooo oooooooooo.    .oooooo.   oooooo   oooooo     oooo ")\e[0m\r"
+        echo -e "${c}$(echo ".888   .Y88.      .888.      .888. .888b.     .8. .888.   .Y8b  d8P.  .Y8b   .888.    .888.     .8.  ")\e[0m\r"
+        echo -e "${c}$(echo " 888   .d88.     .88888.      888   8 .88b.    8   888     888 888      888   .888.   .8888.   .8.   ")\e[0m\r"
+        echo -e "${c}$(echo " 888ooo88P.     .8. .888.     888   8   .88b.  8   888oooo888. 888      888    .888  .8..888. .8.    ")\e[0m\r"
+        echo -e "${c}$(echo " 888.88b.      .88ooo8888.    888   8     .88b.8   888    .88b 888      888     .888.8.  .888.8.     ")\e[0m\r"
+        echo -e "${c}$(echo " 888  .88b.   .8.     .888.   888   8       .888   888    .88P .88b    d88.      .888.    .888.      ")\e[0m\r"
+        echo -ne "${c}$(echo "o888o  o888o o88o     o8888o o888o o8o        .8  o888bood8P.   .Y8bood8P.        .8.      .8.       ")\e[0m\r"
+        sleep 1
+        clear
+    done
+}
+
 #Display rainbow
 index=$(( $1 + 0 ))
 if [[ $1 > "" ]]; then
-  while [[ $index > 0 ]]; do
-    for c in "${colors[@]}"; do
-      echo -ne "${c}$(cat asciirainbow.txt)\e[0m\r"
-      sleep 1
-      clear
+    while [[ $index > 0 ]]; do
+        rainbow
+        (( index-- ))
     done
-  (( index-- ))
-  done
 else
   while true; do
-     for c in "${colors[@]}"; do
-       echo -ne "${c}$(cat asciirainbow.txt)\e[0m\r"
-       sleep 1
-       clear
-      done
-  (( index-- ))
+      rainbow
+      (( index-- ))
   done
 fi
